6c9a8e46347dc2a7e4599d712559d738e15d52e2
lushchan <41105878+lushchan@users.noreply.github.com>
2019-01-07 22:05:15 +0200

    Create wp.sh

---
+++ wp.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+clear
+echo "============================================"
+echo "WordPress Install Script"
+echo "============================================"
+echo "WordPress owner: "
+read -e wpowner
+echo "Database Name: "
+read -e dbname
+echo "Database User: "
+read -e dbuser
+echo "Database Password: "
+stty -echo
+read -e dbpass
+stty echo
+echo "run install? (y/n)"
+read -e run
+if [ "$run" == n ] ; then
+exit
+else
+echo "============================================"
+echo "A robot is now installing WordPress for you."
+echo "============================================"
+#download wordpress
+curl -O https://wordpress.org/latest.tar.gz
+#unzip wordpress
+tar -zxvf latest.tar.gz
+#change dir to wordpress
+cd wordpress
+#copy file to parent dir
+cp -rf . ..
+#move back to parent dir
+cd ..
+#remove files from wordpress folder
+rm -R wordpress
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+#create uploads folder and set permissions
+chown $wpowner:$wpowner $pwd
+mkdir wp-content/uploads
+chmod 777 wp-content/uploads
+echo 'php_flag engine off' >> wp-content/uploads/.htaccess
+#remove zip file
+rm latest.tar.gz
+#remove bash script
+rm wp.sh
+echo "========================="
+echo "Installation is complete."
+echo "========================="
+fi
