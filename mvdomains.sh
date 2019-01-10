7e1ff359ca9673217f625767559c08deac61dd76
lushchan <41105878+lushchan@users.noreply.github.com>
2019-01-10 22:00:37 +0200

    Update mvdomains.sh

--- mvdomains.sh
+++ mvdomains.sh
@@ -7,11 +7,11 @@ while [ "$dir" = "" ]; do
     echo -n "Ticket number for backups dirs. Do not use this script if it is not C7 "
     read dir
     if [ -n "$dir" ]; then
-        mkdir /home/support/lmr/$dir
-        mkdir /home/support/lmr/$dir/domains/
-        mkdir /home/support/lmr/$dir/httpd/
-        mkdir /home/support/lmr/$dir/nginx/
-        mkdir /home/support/lmr/$dir/sql/
+        mkdir -p /home/support/lmr/$dir
+        mkdir -p /home/support/lmr/$dir/domains/
+        mkdir -p /home/support/lmr/$dir/httpd/
+        mkdir -p /home/support/lmr/$dir/nginx/
+        mkdir -p /home/support/lmr/$dir/sql/
     fi
 done
 ####MOVE ACTIONS#######
@@ -20,10 +20,9 @@ do
 mv /home/*/$domain /home/support/lmr/$dir/domains/
 mv /etc/httpd/vhosts.d/*/$domain\.conf /home/support/lmr/$dir/httpd/
 mv /etc/nginx/vhosts.d/*/$domain\.conf /home/support/lmr/$dir/nginx/
-done
 ####CHECK WP DATABASES###
 grep DB_NAME /home/*/$domain/www/wp-config.php | sed "s/define('DB_NAME', '//g" | sed "s/');.*//g" | sed "s/.*\.php://g" > sqlist
-vim sqlist
+done
 cat ./sqlist | while read sqlist
 do
 mysqldump --routines --events --lock-tables $sqlist > /home/support/lmr/$dir/sql/$sqlist\.sql
