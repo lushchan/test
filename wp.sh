2212fec4549010b682853feafbe9d1c8445fe2a7
lmr <41105878+lushchan@users.noreply.github.com>
2019-01-11 23:47:42 +0200

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -1,14 +1,57 @@
 #!/bin/bash -e
+echo "Please choice param -m for manual mode or -f for automatic"
+while [ -n "$1" ]
+do
+case "$1" in
+-m)
 clear
 echo "============================================"
-echo "MYSQL credentials is bellow"
+echo "WordPress Installer manual mode"
+echo "============================================"
+echo "Database Name: "
+read -e dbname
+echo "Database User: "
+read -e dbuser
+echo "Database Password: "
+stty -echo
+read -e dbpass
+stty echo
+echo "run install? (y/n)"
+read -e run
+if [ "$run" == n ] ; then
+exit
+else
+echo "============================================"
+echo "Downloading"
+echo "============================================"
+curl -O https://wordpress.org/latest.tar.gz
+tar -zxvf latest.tar.gz
+cd wordpress
+cp -rf . ..
+cd ..
+rm -R wordpress
+cp wp-config-sample.php wp-config.php
+perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+mkdir wp-content/uploads
+chmod 777 wp-content/uploads
+echo 'php_flag engine off' >> wp-content/uploads/.htaccess
+rm latest.tar.gz
+rm wp.sh
+echo "========================="
+echo "Installation is complete."
+echo "========================="
+fi
+exit;;
+-f)
+clear
+echo "============================================"
+echo "WordPress Installer automatic mode"
 echo "============================================"
 dbname=wp`echo $PWD | cut -d / -f 4| sed -e 's/-/_/g'|sed 's|\.||g'`
 dbuser=wpu`echo $PWD | cut -d / -f 4|cut -c 1-13 | sed 's|-|_|g'|sed 's|\.||g'`
 dbpass=`pwgen 12 1`
-echo "Database Name: $dbname "
-echo "Database User: $dbuser"
-echo "Database Password: $dbpass "
 mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -e "CREATE USER ${dbuser}@localhost IDENTIFIED BY '${dbpass}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbuser}'@'localhost';"
@@ -17,7 +60,6 @@ echo "============================================"
 echo "Install WordPress"
 echo "============================================"
 stty echo
-#typical root-dirs
 DIR=../www
 DIR2=../html
 DIR3=../public_html
@@ -33,34 +75,34 @@ if [ "$run" == n ] ; then
 exit
 else
 echo "============================================"
-echo "Domains - OK."
+echo "Domains - OK. Downloading"
 echo "============================================"
-#download & untar
 curl -O https://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
-#change dir to wordpress
 cd wordpress
-#copy file to parent dir
 cp -rf . ..
-#move back to parent dir
 cd ..
-#remove files from wordpress folder
 rm -R wordpress
-#create wp config
 cp wp-config-sample.php wp-config.php
 perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
 perl -pi -e "s/username_here/$dbuser/g" wp-config.php
 perl -pi -e "s/password_here/$dbpass/g" wp-config.php
-#create uploads folder and set permissions
 wpowner=`stat -c %U .`
 chown -R $wpowner:$wpowner $PWD
 mkdir wp-content/uploads
 chmod 777 wp-content/uploads
 echo 'php_flag engine off' >> wp-content/uploads/.htaccess
-#clean
 rm latest.tar.gz
 rm wp.sh
 echo "========================="
 echo "Installation is complete."
+echo "Database Name: $dbname "
+echo "Database User: $dbuser "
+echo "Database Password: $dbpass "
 echo "========================="
 fi
+exit;;
+*) echo "$1 is not an option" ;;
+esac
+shift
+done                                                                                                                                                                                          108,1         Bo
